0bd44b502ff28543528ce6291f284da5a974f7de
naftalimurgor <murgornaftali@gmail.com>
2021-05-18 20:29:23 +0300

    edit live server start script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Bootsrap_grid
+## Bootstrap grid demo
+
+Install `live server` vscode extension. Enable the extension and click `Go live` from vscode status bar to open the demo on your default favorite (Chrome maybe 😄)browser.
--- liveserver.js
+++ liveserver.js
@@ -5,7 +5,7 @@ var params = {
     host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
     open: true, // When false, it won't load your browser by default.
     ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-   file: "items.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+    file: "items.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
     wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
     mount: [['/components', './node_modules']], // Mount a directory to a route.
     logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
